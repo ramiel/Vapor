1e34148d2a0904b1349d54a39afb0b3840457f00
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2016-11-06 12:12:59 +0100

    Removed useless moment.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,18 +8,6 @@
 
     $(document).ready(function(){
 
-        // On the home page, move the blog icon inside the header
-        // for better relative/absolute positioning.
-
-        //$("#blog-logo").prependTo("#site-head-content");
-
-		$('.post-date').each(function(i, date) {
-			var $date = $(date);
-			$date.html(
-				moment($date.attr('datetime'))
-					.format('dddd, MMMM DD, YYYY')
-			);
-		});
         $(".sticky").sticky({topSpacing:0});
     });
 
